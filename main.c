7a46ce4597443e0c88ac85ee707be67098084958
Omar Abdullah <omar.abdullah@netigo.de>
2017-09-20 22:09:53 +0200

    Created recursive calculator

--- main.c
+++ main.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char getOperator();
+float getNumber();
+float getResult(float number, char operator);
 
 int main() {
-    printf("Hello, World!\n");
+
+    float number;
+    char operator;
+
+    printf("Bitte Zahl eingeben:\n");
+    number = getNumber();
+    operator = getOperator();
+
+    printf("Ergebnis: %f", getResult(number, operator));
     return 0;
+}
+
+char getOperator() {
+    char operator;
+    printf("Operator eingeben:\n");
+    scanf(" %c", &operator);
+    return operator;
+}
+
+float getNumber() {
+    float number;
+    printf("Zahl eingeben:\n");
+    scanf("%f", &number);
+    return number;
+}
+
+float getResult(float number, char operator) {
+    if(operator == '=') {
+        return number;
+    }
+    else {
+        switch(operator)
+        {
+            case '+':
+                number = number + getNumber();
+                break;
+            case '-':
+                number = number - getNumber();
+                break;
+            case ' ':
+                break;
+            default:
+                printf("Kein Operator eingegeben!");
+                exit(0);
+        }
+        operator = getOperator();
+        number = getResult(number, operator);
+    }
+    return number;
 }
